6fda8e7815a3d36b101b779bfd5647e0ef589527
Geon Lee <63420948+geon8692@users.noreply.github.com>
2023-12-17 19:52:17 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,7 @@
+code = list(map(int, input().split()))
+if code == sorted(code):
+    print('ascending')
+elif code == sorted(code, reverse=True):
+    print('descending')
+else:
+    print('mixed')
