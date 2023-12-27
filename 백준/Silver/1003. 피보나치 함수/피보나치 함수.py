d8bebeef0c85292ee50b45053fa601e938ec284c
Geon Lee <63420948+geon8692@users.noreply.github.com>
2023-12-27 23:35:26 +0900

    [Silver III] Title: 피보나치 함수, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,13 @@
+def fibo(n):
+    zero = [1, 0, 1]
+    one = [0, 1, 1]
+    if n >= 3: 
+        for i in range(3, n+1):
+            zero.append(zero[i-2] + zero[i-1])
+            one.append(one[i-2] + one[i-1])
+    print(zero[n], one[n])
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    fibo(n)
