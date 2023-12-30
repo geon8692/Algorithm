9e61d4b2ab69ac3ebc6e6c4b8cf092532c116935
Geon Lee <63420948+geon8692@users.noreply.github.com>
2023-12-30 23:19:31 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -4,14 +4,10 @@ for _ in range(T):
     k = int(input())
     n = int(input())
     
-    dp = [[0] * (n+1) for _ in range(k+1)]
-    for i in range(1, n+1):
-        dp[0][i] = i
-    for i in range(1, k+1):
-        dp[i][1] = 1
+    dp = list(range(1, n+1))
     
-    for i in range(1, k+1):
-        for j in range(1, n+1):
-            dp[i][j] = dp[i][j-1] + dp[i-1][j]
-    print(dp[k][n])
-        
+    for _ in range(k):
+        for i in range(1, n):
+            dp[i] += dp[i-1]
+    print(dp[n-1])
+
